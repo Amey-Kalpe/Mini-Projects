2102e879f8be52a00d040adc1a57714270d9587c
Amey Sanjay Kalpe <amey.kalpe@abzooba.com>
2023-02-08 16:26:46 +0530

    Implemented register route

--- main.py
+++ main.py
@@ -1,82 +1,86 @@
-from datetime import datetime
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap
+from flask_ckeditor import CKEditor
+from datetime import date
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, URL
-from flask_ckeditor import CKEditor, CKEditorField
-
-
-## Delete this code:
-# import requests
-# posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
-
+from sqlalchemy.orm import relationship
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from forms import CreatePostForm, RegistrationForm
+from flask_gravatar import Gravatar
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
-app.config["CKEDITOR_PKG_TYPE"] = "basic"
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///posts.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-## util functions
-def row_to_dict(rows):
-    return [
-        {
-            "id": row.id,
-            "title": row.title,
-            "subtitle": row.subtitle,
-            "date": row.date,
-            "body": row.body,
-            "author": row.author,
-            "img_url": row.img_url,
-        }
-        for row in rows
-    ]
+##CONFIGURE TABLES
 
-
-##CONFIGURE TABLE
 class BlogPost(db.Model):
+    __tablename__ = "blog_posts"
     id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(250), nullable=False)
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(250), nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
 
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
-##WTForm
-class CreatePostForm(FlaskForm):
-    title = StringField("Blog Post Title", validators=[DataRequired()])
-    subtitle = StringField("Subtitle", validators=[DataRequired()])
-    author = StringField("Your Name", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = CKEditorField("Blog Content", validators=[DataRequired()])
-    submit = SubmitField("Submit Post")
-
-
-def fetch_posts():
-    return row_to_dict(BlogPost.query.all())
+db.create_all()
 
 
-@app.route("/")
+@app.route('/')
 def get_all_posts():
-    return render_template("index.html", all_posts=fetch_posts())
+    posts = BlogPost.query.all()
+    return render_template("index.html", all_posts=posts)
 
 
-@app.route("/post/<int:index>")
-def show_post(index):
-    requested_post = None
-    for blog_post in fetch_posts():
-        if blog_post["id"] == index:
-            requested_post = blog_post
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    registration_form = RegistrationForm()
+    if request.method == "POST":
+        if registration_form.validate_on_submit():
+            req_form = request.form
+            name = req_form.get("name")
+            email = req_form.get("email")
+            password = req_form.get("password")
+            user = User(
+                name=name,
+                email=email,
+                password=generate_password_hash(password, salt_length=8)
+            )
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for("get_all_posts"))
+    return render_template("register.html", form=registration_form)
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for('get_all_posts'))
+
+
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    requested_post = BlogPost.query.get(post_id)
     return render_template("post.html", post=requested_post)
 
 
@@ -85,58 +89,58 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/edit-post/<post_id>", methods=["GET", "POST"])
-def edit_post(post_id):
-    post = BlogPost.query.get(post_id)
-    form = CreatePostForm(
-        title=post.title,
-        subtitle=post.subtitle,
-        body=post.body,
-        author=post.author,
-        img_url=post.img_url,
-    )
-    if form.validate_on_submit():
-        post.subtitle = form.subtitle.data
-        post.img_url = form.img_url.data
-        post.body = form.body.data
-        post.author = form.author.data
-        db.session.commit()
-        return redirect(url_for("show_post", index=post.id))
-
-    return render_template("make-post.html", form=form, header="Edit Post")
-
-
-@app.route("/new-post", methods=["GET", "POST"])
-def create_post():
-    form = CreatePostForm()
-    if request.method == "POST":
-        if form.validate_on_submit():
-            req_form = request.form
-            post = BlogPost(
-                title=req_form.get("title"),
-                subtitle=req_form.get("subtitle"),
-                date=datetime.now().strftime("%B %d, %Y"),
-                author=req_form.get("author"),
-                body=req_form.get("body"),
-                img_url=req_form.get("img_url"),
-            )
-            db.session.add(post)
-            db.session.commit()
-            return redirect(url_for("get_all_posts"))
-    return render_template("make-post.html", form=form, header="New Post")
-
-
-@app.route("/delete/<int:post_id>")
-def delete_post(post_id):
-    BlogPost.query.filter_by(id=post_id).delete()
-    db.session.commit()
-    return redirect(url_for("get_all_posts"))
-
-
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
 
 
+@app.route("/new-post")
+def add_new_post():
+    form = CreatePostForm()
+    if form.validate_on_submit():
+        new_post = BlogPost(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            body=form.body.data,
+            img_url=form.img_url.data,
+            author=current_user,
+            date=date.today().strftime("%B %d, %Y")
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for("get_all_posts"))
+    return render_template("make-post.html", form=form)
+
+
+@app.route("/edit-post/<int:post_id>")
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        post.author = edit_form.author.data
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for("show_post", post_id=post.id))
+
+    return render_template("make-post.html", form=edit_form)
+
+
+@app.route("/delete/<int:post_id>")
+def delete_post(post_id):
+    post_to_delete = BlogPost.query.get(post_id)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for('get_all_posts'))
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
